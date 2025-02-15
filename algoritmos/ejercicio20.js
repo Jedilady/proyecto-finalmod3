5961ffd29ccfd98586010444f233a0103f3b8bc3
Jedilady <jedilady@gmail.com>
2025-02-15 16:18:31 +0000

    Solucion ej 20

--- algoritmos/ejercicio20.js
+++ algoritmos/ejercicio20.js
@@ -6,18 +6,138 @@
 //– Hay 1000 millisegundos en un segundo
 //– Podemos obtener la hora, minutos y segundos con la función "getTime"
 
-//Obtenemos la hora actual:
-let today = new Date();
 
-//Obtenemos la hora de hoy a las 00:00:00 con el método "Set Today"
-//Esto ya setea ms a cero
-let horaCero = today.setHours(0, 0, 0, 0);
 
-//Obtenemos los ms que han transcurrido HOY con el método valueOf
-const valueDateToday = dateToday.valueOf();
+//La forma sencilla de resolver el reloj: usando getHours, getMinutes y getSeconds
+
+console.log("Primera función: usando getHours, getMinutes y getSeconds + getTime y setHours");
+
+
+//Establecemos la hora actual con new Date
+function easyClock (){
+    //variable para obtener la hora actual
+    let currentTime = new Date();
+
+    //Variables par aobtener la hora, minutos y segundos a partir de la hora actual
+    let h = currentTime.getHours();
+    let m = currentTime.getMinutes();
+    let s = currentTime.getSeconds();
+
+    //creamos una función para transformar el formato a dos dígitos con "padStart"
+    let formattedTime = (number) => (String(number).padStart(2, '0'));
+
+    let hh = formattedTime(h);
+    let mm = formattedTime(m);
+    let ss = formattedTime(s);
+    
+    //variable para setear la hora de hoy en cero
+    let midnight = new Date().setHours(0, 0, 0, 0);//Ver nota abajo
+    //Nos devuelve el total de ms desde el epoch hasta la hora seteada (00:00:00:00)
+    //console.log(midnight);
+
+    //Variable para obtener los ms desde el epoch hasta el momento actual:
+    let currentMS = currentTime.getTime();
+
+    //variable que almacena la resta de los ms totales - los de la media noche:
+    let todayMS = currentMS - midnight;
+    //console.log(currentMS, midnight);
+    
+    //Retorno final de la función:
+    return (console.log(`La hora actual es: ${hh}:${mm}:${ss}`) + console.log(`Y los milisegundos transcurridos hoy: ${todayMS}`));
+
+    //Nota: para obtener la hora reseteada, y no los ms, hacemos así:
+    
+    //let hora = new Date(); hora.setHours(0, 0, 0, 0); console.log(hora)
+
+    // Creamos una NUEVA variable para la fecha actual, y le pasamos setHours SIN guardarlo en una variable. 
+    // Esto es porque setHours *modifica directamente* lo que hay en la variable de la fecha, y al llamar a dicha variable, obtendremos la variable ahora modificada. 
+    // Pero si guardamos esto en una variable y la llamamos, nos dará el resultado en ms, que es lo que devuelve en realidad setHours
+    
+}
+
+easyClock();
+
+//Calculando la hora a partir de  getTime
+
+console.log(" ");
+
+console.log("Segunda función: realizando el cálculo de la hora a partir de los ms obtenidos con getTime y setHours");
+
+function msClock() {
+    //creamos una variable para obtener la fecha actual
+    let currentTime = new Date();
+
+    // variable para obtener los ms totales
+    let currentMS = currentTime.getTime();
+
+    // variable para los ms hasta media noche 
+    let midnight = new Date().setHours(0, 0, 0, 0);
+
+    //variable que almacena la resta de los ms totales - los de la media noche:
+    let todayMS = currentMS - midnight;
+
+    //convertimos los milisegundos en segundos
+    let todayS = Math.floor(todayMS / 1000);
+
+    // la hora la obtenemos con el entero de los segundos totales entre 3600
+    let hour = Math.floor(todayS / 3600);
+    //lo convertimos a formato 00 con slice:
+    let formattedHour = ("0" + hour).slice(-2);
+    //los convertimos en segundos de nuevo para poder calcular los minutos
+    let hourSecs = hour * 3600
+
+    // los minutos los obtenemos restando los segundos correspondientes a las horas, y el resto obteniendo el entero de la división / 60
+    let min = Math.floor((todayS - hourSecs) / 60);
+    //lo convertimos a formato 00 con slice:
+    let formattedMin = ("0" + min).slice(-2);
+    //convertimos de nuevo los minutos a segundos para poder obtener el resto de segundos
+    let minSecs = min * 60
+
+    //restamos el total en segundos de horas y minutos al total de segundos
+    let sec = todayS - hourSecs - minSecs;
+    //lo convertimos a formato 00 con slice:
+    let formattedSec = ("0" + sec).slice(-2);
+
+    return console.log(`La hora actual es: ${formattedHour}:${formattedMin}:${formattedSec}`) + console.log(`Y los milisegundos transcurridos hoy: ${todayMS}`);
+    
+    // Sabemos que los milisegundos transcurridos en un día son igual a:
+    // ((h * 3600) + (m * 60) + s) * 1000
+    // es decir, que si dividimos el total de ms del día entre 1000, tendremos la cantidad total de segundos del día
+    // luego, para saber la cantidad de horas, tomamos el total de segundos / 3600, y redondeamos hacia abajo
+    // para los minutos, multiplicamos las horas por 3600, las restamos al total de segundos, y dividimos entre 60, redondeando hacia abajo
+    // los segundos restantes será el resultados de restar la cantidad de horas * 3600 - la cantidad de minutos * 60
+}
+
+msClock();
+
+
+
+
+// NOTA PERSONAL
+// Procesos varios para conseguir estos dos resultados
+
+/*
+//Obtenemos la fecha y hora actual, asignada a una constante:
+const todayTime = new Date();
+console.log("Hoy es:", todayTime);
+
+//Obtenemos los ms transcurridos hasta el día de hoy, a las 00:00:00 con el método "setHours"
+//creamos una nueva variable para la hora actual a la que le aplicaremos setHours. 
+//si lo hacemos a "todayTime", cambiaremos la variable, y eso no nos interesa
+const todayTimeToReset = new Date()
+console.log("Hoy2", todayTimeToReset);
+//cómo funciona esto?
+//
+
+//Obtenemos los ms que han transcurrido hasta el momento actual con el método "valueOf" aplicado a la variable con la hora actual
+let valueDateToday = todayTime.valueOf();
+console.log("valueDateToday", valueDateToday);
 
 //restamos los milisegundos transcurridos menos los seteados a las 00:00:00
-console.log(valueDateToday - valueHoraCero); 
+let todayMilisecs = valueDateToday - valueHoraCero;
+console.log("this", todayMilisecs); 
+console.log("this");
+*/
 
 /*
 console.log("Reloj: ");
@@ -30,8 +150,13 @@ console.log( date.getHours() );
 console.log( date.getMinutes() );
 
 console.log( date.getSeconds() );
+*/
 
+
+/*
 let today = new Date();
+console.log(today);
+
 
 today.setHours(0, 0, 0, 0);
 console.log(today); 
